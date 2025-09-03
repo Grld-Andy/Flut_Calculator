10f41064ad7424b876fb25c8711c9fa47855a5a8
Andy Ansong <andyansong82@gmail.com>
2025-09-03 09:27:35 +0000

    style: set background color for button

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -12,32 +12,41 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
-      body: Column(
-        children: [
-          GridView.count(
-            crossAxisCount: 4,
-            mainAxisSpacing: 10,
-            crossAxisSpacing: 10,
-            children: [
-              TextButton(
-                onPressed: (){},
-                child: Text("+")
-              ),
-              TextButton(
-                onPressed: (){},
-                child: Text("+")
-              ),
-              TextButton(
-                onPressed: (){},
-                child: Text("+")
-              ),
-              TextButton(
-                onPressed: (){},
-                child: Text("+")
-              ),
-            ],
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            GridView.count(
+              crossAxisCount: 4,
+              mainAxisSpacing: 10,
+              crossAxisSpacing: 10,
+              shrinkWrap: true,
+              children: [
+                TextButton(
+                  onPressed: (){},
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.grey[600],
+                  ),
+                  child: const Text(
+                    "+"
+                  )
+                ),
+                TextButton(
+                  onPressed: (){},
+                  child: const Text("+")
+                ),
+                TextButton(
+                  onPressed: (){},
+                  child: const Text("+")
+                ),
+                TextButton(
+                  onPressed: (){},
+                  child: const Text("+")
+                ),
+              ],
+            ),
+          ],
+        ),
       )
     );
   }
