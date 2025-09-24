bbe458a220d35eb99f8786edf011088da7bb8e95
Andy Ansong <andyansong82@gmail.com>
2025-09-24 11:22:30 +0000

    feat: generate buttons

--- lib/screens/main_view.dart
+++ lib/screens/main_view.dart
@@ -5,20 +5,56 @@ class MainView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> buttons = [
+      "%", "(", ")", "÷",
+      "7", "8", "9", "*",
+      "6", "5", "4", "-",
+      "3", "2", "1", "+",
+      "C", "0", ".", "="
+    ];
+
     return Scaffold(
       backgroundColor: Colors.grey[900],
-      body: Column(
-        children: [
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 4,
-              mainAxisSpacing: 10,
-              crossAxisSpacing: 10,
-              padding: const EdgeInsets.all(10),
-              children: List.generate(8, (index) => Container(color: Colors.blueGrey)),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            // display screen
+              Column(
+                children: [
+                  Text("5 + 3"),
+                  Text("8")
+                ],
+              ),
+        
+            // buttons grid
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10
+                ),
+                itemCount: buttons.length,
+                itemBuilder: (context, index){
+                  return TextButton(
+                    onPressed: (){},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.grey[800],
+                    ),
+                    child: Text(
+                      buttons[index],
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20
+                      ),
+                    ),
+                  );
+                },
+              )
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
