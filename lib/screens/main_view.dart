1c04e81e96142c9a855f5dc9a2d0cb9e8ad657a1
Andy Ansong <andyansong82@gmail.com>
2025-11-11 02:44:35 +0000

    catch evaluation errors

--- lib/screens/main_view.dart
+++ lib/screens/main_view.dart
@@ -9,7 +9,6 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
-
   String calculation = "";
   String result = "0";
 
@@ -36,16 +35,15 @@ class _MainViewState extends State<MainView> {
         case "=":
           if (calculation.isEmpty) return;
           try {
-            // Replace special operators for FunctionTree
             String expression = calculation.replaceAll('÷', '/');
             final eval = expression.interpret();
             result = eval.toString();
           } catch (e) {
+            print("error $e");
             result = "Error";
           }
           break;
         case "()":
-          // Auto-detect whether to insert '(' or ')'
           int openParens = '('.allMatches(calculation).length;
           int closeParens = ')'.allMatches(calculation).length;
           calculation += openParens == closeParens ? '(' : ')';
@@ -54,99 +52,106 @@ class _MainViewState extends State<MainView> {
           calculation += val;
       }
     });
-  }  
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.grey[900],
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            // display screen
-            Expanded(
-              flex: 2,
-              child: Container(
-                alignment: Alignment.bottomRight,
-                padding: EdgeInsets.all(5.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  color: Colors.grey[800],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Text(
-                      calculation,
-                      style: TextStyle(
-                        color: Colors.white70,
-                        fontSize: 20
-                      ),
+      body: SafeArea(
+        child: SizedBox.expand(
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                // display screen
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    alignment: Alignment.bottomRight,
+                    padding: const EdgeInsets.all(5.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      color: Colors.grey[800],
                     ),
-                    Text(
-                      result,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30
-                      ),
-                    ),
-                  ],
-                ),
-              )
-            ),
-            SizedBox(height: 10,),
-            // buttons grid
-            Expanded(
-              flex: 5,
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10
-                ),
-                itemCount: buttons.length,
-                itemBuilder: (context, index){
-                  final buttonText = buttons[index];
-                  bool isOperator = ["+", "-", "*", "÷", "%", "=", "()", ".", "C", "<"].contains(buttonText);
-                  if(buttonText == "<"){
-                    return IconButton(
-                      onPressed: (){
-                        takeInput(buttonText);
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: Colors.blue[900] 
-                      ),
-                      icon: const Icon(
-                        Icons.backspace,
-                        color: Colors.white,
-                      ),
-                    );
-                  }else{
-                    return TextButton(
-                      onPressed: (){
-                        takeInput(buttonText);
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: buttonText == "C" ? Colors.red[700] :
-                          isOperator ? Colors.blue[900] : 
-                          Colors.grey[800],
-                      ),
-                      child: Text(
-                        buttonText,
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 25
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        Text(
+                          calculation,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 40,
+                          ),
                         ),
-                      ),
-                    );
-                  }
-                },
-              )
+                        Text(
+                          result,
+                          style: const TextStyle(
+                            color: Colors.white70,
+                            fontSize: 30,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                // buttons grid
+                Expanded(
+                  flex: 5,
+                  child: GridView.builder(
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemCount: buttons.length,
+                    itemBuilder: (context, index) {
+                      final buttonText = buttons[index];
+                      bool isOperator = [
+                        "+", "-", "*", "÷", "%", "=", "()", ".", "C", "<"
+                      ].contains(buttonText);
+                      if (buttonText == "<") {
+                        return IconButton(
+                          onPressed: () {
+                            takeInput(buttonText);
+                          },
+                          style: TextButton.styleFrom(
+                            backgroundColor: Colors.blue[900],
+                          ),
+                          icon: const Icon(
+                            Icons.backspace,
+                            color: Colors.white,
+                          ),
+                        );
+                      } else {
+                        return TextButton(
+                          onPressed: () {
+                            takeInput(buttonText);
+                          },
+                          style: TextButton.styleFrom(
+                            backgroundColor: buttonText == "C"
+                                ? Colors.red[700]
+                                : isOperator
+                                    ? Colors.blue[900]
+                                    : Colors.grey[800],
+                          ),
+                          child: Text(
+                            buttonText,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                            ),
+                          ),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
